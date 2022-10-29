ea6e6819d06085e2f8465ae4d2cf93c4dfdccabf
lyseebean <112133626+srfehr@users.noreply.github.com>
2022-10-29 10:27:12 -0500

    Courses

---
+++ src/course/CollegeCourse.java
@@ -0,0 +1,25 @@
+package course;
+
+public class CollegeCourse {
+	String department;
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getCourseNumber() {
+		return courseNumber;
+	}
+	public void setCourseNumber(int courseNumber) {
+		this.courseNumber = courseNumber;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	int courseNumber, cost;
+	
+}
